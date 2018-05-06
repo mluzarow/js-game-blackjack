8b41f4ca2599cf2418fdd579ab4d09c1e85b2476
Mackenzie <ma.luzarow@gmail.com>
2018-05-05 23:24:34 -0500

    Added bet mechanic.

--- js/BlackJack.js
+++ js/BlackJack.js
@@ -13,6 +13,24 @@ class BlackJack {
 		$double.addEventListener ("click", this.buttonDoubleClick.bind (this));
 		$surrender.addEventListener ("click", this.buttonSurrenderClick.bind (this));
 		
+		this.$playerFunds = document.getElementsByClassName ("player_funds_value")[0];
+		
+		this.totalFunds = 50000;
+		this.currentBet = 10;
+		
+		let $currentBet = document.getElementsByClassName ("bet_box_value")[0];
+		let $arrowUp = document.getElementsByClassName ("arrow_up")[0];
+		let $arrowDown = document.getElementsByClassName ("arrow_down")[0];
+		
+		$arrowUp.addEventListener ("click", this.buttonArrowUpClick.bind (this, $currentBet));
+		$arrowDown.addEventListener ("click", this.buttonArrowDownClick.bind (this, $currentBet));
+		
+		let $lock_in = document.getElementsByClassName ("lock_in")[0];
+		let $betValueWrap = document.getElementsByClassName ("bet_value_wrap")[0];
+		let $betValue = document.getElementsByClassName ("bet_value")[0];
+		
+		$lock_in.addEventListener ("click", this.buttonLockInClick.bind (this, $betValueWrap, $betValue));
+		
 		this.$controls = document.getElementsByClassName ("control_wrap")[0];
 		
 		this.$cardAreaPlayer = document.getElementsByClassName ("player_card_area")[0];
@@ -29,8 +47,6 @@ class BlackJack {
 		
 		this.playerTotal = 0;
 		this.houseTotal = 0;
-		
-		this.startGame ();
 	}
 	
 	startGame () {
@@ -80,6 +96,31 @@ class BlackJack {
 	
 	buttonSurrenderClick () {
 		
+	}
+	
+	buttonLockInClick ($wrap, $bet) {
+		$bet.innerHTML = this.currentBet;
+		$wrap.classList.add ("locked");
+		
+		this.totalFunds -= this.currentBet;
+		
+		this.$playerFunds.innerHTML = this.totalFunds;
+		
+		this.startGame ();
+	}
+	
+	buttonArrowUpClick ($bet) {
+		if (this.currentBet <= this.totalFunds) {
+			this.currentBet += 10
+			$bet.innerHTML = this.currentBet;
+		}
+	}
+	
+	buttonArrowDownClick ($bet) {
+		if (this.currentBet > 10) {
+			this.currentBet -= 10
+			$bet.innerHTML = this.currentBet;
+		}
 	}
 	
 	appendCardNodeHouse (card, flipped = false) {
